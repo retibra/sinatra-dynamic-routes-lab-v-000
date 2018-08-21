82c6ae8f86fbe87c163c2f2fd668db5cb4622e0e
Timothy Ege <retibra@gmail.com>
2018-08-21 01:55:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,6 +30,16 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+    @sum = @number1 + @number2
+    @difference = @number1 - @number2
+    @product = @number1*@number2
+    @quotient = @number1/@number2
 
+    "#{@sum}"
+    "#{@difference}"
+    "#{@product}"
+    "#{@quotient}"
   end
 end
